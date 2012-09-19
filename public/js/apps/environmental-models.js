33cdcd56d8863d6c556110883777b3b1ede4ba2e
anarosner <ana@alumni.tufts.edu>
2012-09-19 11:15:26 -0300

    Update public/js/apps/environmental-models.js

--- public/js/apps/environmental-models.js
+++ public/js/apps/environmental-models.js
@@ -1,6 +1,6 @@
 Streams.app_control.apps.environmental_models = {
   name : 'Environmental Models',
-  order: 4,
+  order: 3,
   init : function () {
     //// Initialize View ////
     var view = $('<div id="environmental-models-app">');

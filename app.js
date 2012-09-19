da0c759b091fdaffd15f462473a17ca35165aac2
Tim Richards <tim.d.richards@gmail.com>
2012-09-19 16:54:05 -0400

    Added configuration file streams.json to configure alternate servers. At the moment this file specifies the port number for the application and information related to the execution and output of RScripts.  Also converted the server-side weather model to an Object that can be initialized with this configuration. Changed the routes in app.js to reflect these changes and handle the routes accordingly.

--- app.js
+++ app.js
@@ -4,6 +4,13 @@ var express = require('express'),
     reach   = require('./lib/reach'),
     models  = require('./lib/models');
 
+// Load the application configuration file:
+var streamsConfig = require('./streams.json');
+
+// Display config file for debugging purposes:
+console.log('Streams Configuration:');
+console.log(streamsConfig);
+
 // Possibly add DB support in future?
 /*
 var db = require("mysql-native").createTCPClient('instance12373.db.xeround.com', 8392); // localhost:3306 by default
@@ -42,14 +49,24 @@ app.configure('production', function(){
   app.use(express.errorHandler());
 });
 
+// Create a weather model object:
+var weatherModel = models.weatherModel(streamsConfig);
+
 // Routes
 app.get('/', routes.main);
-app.get('/weather-model-exec', models.weather_model_exec);
-app.get('/weather-model-exec/status', models.weather_model_status);
+// Weather Model Routes:
+app.get('/weather-model-exec',
+  function (req, res) {
+    weatherModel.exec(req, res);
+  });
+app.get('/weather-model-exec/status',
+  function (req, res) {
+    weatherModel.status(req, res);
+  });
 
 // app.get('/v2', routes.v2);
 
-app.listen(process.env.PORT || process.env.C9_PORT || 9999);
+app.listen(process.env.PORT || process.env.C9_PORT || streamsConfig.port);
 console.log("Express server listening on port %d in %s mode",
             app.address().port, app.settings.env);
 

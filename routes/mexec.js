f4c7e37bb93c03c72a811bc74050e8038dfa2c8d
Kun <captorxp@gmail.com>
2012-12-26 13:17:03 -0500

    mexec.js is modified
    codes are added fro 1)making directories for each run. 2) create setting
    info for each run, so that R scripts can ready the parameters. 3) call
    the runModels() to execute the R scripts.
    Todo: directories containing the R scripts and all the users (not a
    single user) are not decided yet. these paths are set to 'xxxxxx'
    the data inside the request body is not clear yet. The code assume that
    req.body has an array of runs, where each element in the array contains
    the runId, scriptName, and so on.

--- routes/mexec.js
+++ routes/mexec.js
@@ -1,13 +1,35 @@
 var users = require('lib/users');
 var mexec = require('lib/mexec');
+var fs = require('fs');
 
 // Routes for the mexec library.
-exports.exec = function (req, res) {
-  var settings = req.body;
-  var user     = req.session.user;  // TODO: assign user (login?)
-  // TODO:
-  // - get the user
-  // - get a new run directory
-  // - create settings file
-  // - call mexec.runModels
+exports.exec = function(req, res) {
+	var settings = req.body;
+	var user = req.session.user; // TODO: assign user (login?)
+	var runReady = true;
+	// TODO:
+	// - get the user
+	// - get a new run directory for each run
+	var runs = settings.runs;
+	for(var aRun in runs) {
+		var path = 'xxxusers/' + user + '/' + aRun.scriptName + '/';
+		aRun.runId;
+		mkdir_p(path);
+		// - create settings file
+		fs.writeFile(path+'/'+aRun.scriptName+'.setting', aRun, function(err) {
+			if(err) {
+				console.log("cannot save setting for the model: " + aRun.scriptName);
+				//TODO: remove the path with nodejs
+
+				runReady = false;
+				throw err;
+				break;
+			}
+			console.log("setting for " + aRun.scriptName + " is saved");
+		});
+	}
+	// - call mexec.runModels
+	if(runReady) {
+		mexec.runModels();
+	}
 };

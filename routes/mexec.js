6b4b3b3c28c755054bd8b0d9b408c8615619fe82
KunKun <kunkun@tx1000.(none)>
2013-01-11 12:03:45 -0500

    functions that perform Basin ID-Alias read/write operation for a specific user  are added in lib/basin/basin.js

--- routes/mexec.js
+++ routes/mexec.js
@@ -1,6 +1,5 @@
 var users = require('../lib/users');
 var mexec = require('../lib/mexec');
-// var tools = require('./toolbox.js');
 var fs = require('fs');
 
 // Routes for the mexec library.
@@ -19,32 +18,32 @@ exports.exec = function(req, res) {
 	
 	// - get a new run directory for each run
 	
-	for( var idx =0; idx<runs.length;idx++ ) {
-		var aRun = runs[idx];
-		//generate a runId with uuid
-		if(aRun.runId===''){
-			aRun.runId = 1234;
-		}
-		var path = __dirname + '/../lib/mexec' + user + '/' + aRun.scriptName + '/'+aRun.runId;
-		mexec.mkdir_p(path);
+	// for( var idx =0; idx<runs.length;idx++ ) {
+	// 	var aRun = runs[idx];
+	// 	//generate a runId with uuid
+	// 	if(aRun.runId===''){
+	// 		aRun.runId = 1234;
+	// 	}
+	// 	var path = __dirname + '/../lib/mexec' + user + '/' + aRun.scriptName + '/'+aRun.runId;
+	// 	mexec.mkdir_p(path);
 
-		// - create settings file
-		fs.writeFile(path+'/'+aRun.scriptName+'.setting', aRun, function(err) {
-			console.log(err);
-			if(err) {
-				console.log("cannot save setting for the model: " + aRun.scriptName);
-				//TODO: remove the path with nodejs-done
-				mexec.rimraf(path,function(err){
-					if(err){
-						console.log("cannot remove path: "+path);
-					}
-				});
-				throw err;
-				//return;
-			}
-			console.log("setting for " + aRun.scriptName + " is saved");
-		});
-	}
+	// 	// - create settings file
+	// 	fs.writeFile(path+'/'+aRun.scriptName+'.setting', aRun, function(err) {
+	// 		console.log(err);
+	// 		if(err) {
+	// 			console.log("cannot save setting for the model: " + aRun.scriptName);
+	// 			//TODO: remove the path with nodejs-done
+	// 			mexec.rimraf(path,function(err){
+	// 				if(err){
+	// 					console.log("cannot remove path: "+path);
+	// 				}
+	// 			});
+	// 			throw err;
+	// 			//return;
+	// 		}
+	// 		console.log("setting for " + aRun.scriptName + " is saved");
+	// 	});
+	// }
 	// - call mexec.runModels
 	if(runReady) {
 		mexec.runModels(runs,0,"../lib/users");

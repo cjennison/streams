558fc794b1c5d490fe1cddee7e4ff4f89b170811
kuntu <captorxp@gmail.com>
2013-01-15 16:18:54 -0500

    add datadir to basin.js

--- routes/mexec.js
+++ routes/mexec.js
@@ -5,11 +5,11 @@ var fs = require('fs');
 // Routes for the mexec library.
 exports.exec = function(req, res) {
 	var settings = req.body;
-	var user = req.session.user; 
+	var user = req.session.user;
 	//build up an array of runs from the setting
-	var runs = mexec.buildRuns(settings,user);	
+	var runs = mexec.buildRuns(settings,user);
 	
-	if(runs.length>0) {
+	if(runs.length>0) {//TODO: set the directory for the r scripts
 		mexec.runModels(runs,0,"../lib/users");
 	}
 };

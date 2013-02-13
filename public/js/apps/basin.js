c57230e7411221be3900d1ebebf31ffbbb061683
Chris Jennison <cjennison@hitpointstudios.com>
2013-02-12 20:58:36 -0500

    debug Off

--- public/js/apps/basin.js
+++ public/js/apps/basin.js
@@ -167,7 +167,7 @@ Streams.app_control.apps.basin = {
 	     		
 	     });
      
-    changeView("name", 'id');
+    //changeView("name", 'id');
 
    }
    //Starts Loading JSON Object from server
